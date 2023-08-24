d44afaeaa4fa4b676891ec212e280066e7d09739
Stanisz1 <atrohov97@gmail.com>
2023-08-24 19:49:30 +0300

    terraform variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,18 +1,59 @@
 variable "deployment" {
   description = "Deployment-related variables"
-  default = {
-    app_name        = "devops_exercise_"
-    name            = "devops_exercise_"
+  redis = {
+    app_name        = "redis"
+    name            = "redis"
     replica_number  = 1
-    container_image = "ghcr.io/stanisz1/devops_exercise_:ace07b74b63cab357e1623f20a19ccf99b6ad187"
-    container_name  = "devops_exercise_"
+    container_image = "ghcr.io/stanisz1/redis:0.3.0"
+    container_name  = "redis"
   }
+
+  back = {
+    app_name        = "back"
+    name            = "back"
+    replica_number  = 1
+    container_image = "ghcr.io/stanisz1/back:0.3.0"
+    container_name  = "back"
+  }
+
+  front = {
+    app_name        = "front"
+    name            = "front"
+    replica_number  = 1
+    container_image = "ghcr.io/stanisz1/front:0.3.0"
+    container_name  = "front"
+  }
+
+  nginx = {
+    app_name        = "nginx"
+    name            = "nginx"
+    replica_number  = 1
+    container_image = "ghcr.io/stanisz1/nginx:0.3.0"
+    container_name  = "nginx"
+  }
+
+
 }
 
 variable "service" {
   description = "Service-related variables"
-  default = {
-    name        = "devops_exercise_service"
+  redis = {
+    name        = "redisservice"
+    port        = 6379
+    target_port = 6379
+  }
+  back = {
+    name        = "backservice"
+    port        = 4000
+    target_port = 4000
+  }
+  front = {
+    name        = "frontservice"
+    port        = 3000
+    target_port = 3000
+  }
+  nginx = {
+    name        = "nginxservice"
     port        = 80
     target_port = 80
   }
@@ -20,15 +61,32 @@ variable "service" {
 
 variable "ingress" {
   description = "Ingress-related variables"
-  default = {
-    name      = "devops_exercise_ingress"
-    host      = "devops_exercise_terraformhw.com"
-    port      = 80
+  redis = {
+    name      = "redisingress"
+    host      = "redisterra.com"
+    port      = 6379
+  }
+  back = {
+    name      = "backingress"
+    host      = "backterra.com"
+    port      = 4000
+    path      = "/api"
+    path_type = "Prefix"
+  }
+  front = {
+    name      = "frontingress"
+    host      = "frontterra.com"
+    port      =  3000
     path      = "/"
     path_type = "Prefix"
+  }
+  nginx = {
+    name      = "nginxingress"
+    host      = "nginxterra.com"
+    port      = 80
   }
 }
 
 variable "lables_app_name" {
-  default = "devops_exercise_"
+  default = "latest"
 }
