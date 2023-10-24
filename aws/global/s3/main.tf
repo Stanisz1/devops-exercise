9a38f8eb7cb538107d659899291a84a26f58fbc8
Stanisz1 <runner@fv-az216-141.pt1oxs52cqpu1cpb5jojekw3tg.jx.internal.cloudapp.net>
2023-10-24 13:57:27 +0000

    Delete s3 bucket tf.state from aws

--- aws/global/s3/main.tf
+++ aws/global/s3/main.tf
@@ -49,10 +49,4 @@ resource "aws_s3_bucket_public_access_block" "public_access" {
 terraform {
  }
 terraform {
-   backend "s3" {
-    bucket         = "devops-exersice-staniz-s3"
-    key            = "devops-exersice-staniz-s3/global/s3/terraform.tfstate"
-    region         = "eu-north-1"
-    encrypt        = true
-  }
  }
