46e2ff46e4253e909052dbd37696157bcabe5c33
Stanislaw <you@example.com>
2023-10-26 12:12:06 +0200

    aws 1

--- aws/stage/services/frontend-app/main.tf
+++ aws/stage/services/frontend-app/main.tf
@@ -440,7 +440,7 @@ resource "aws_ecs_service" "front" {
 
   network_configuration {
     security_groups  = [aws_security_group.ecs_tasks.id]
-    subnets          = concat(aws_subnet.public_subnets_b[*].id)
+    subnets          = concat(aws_subnet.public_subnets_b[*].id,)
     assign_public_ip = true
   }
 
@@ -578,7 +578,7 @@ resource "aws_ecs_service" "back" {
 
   network_configuration {
     security_groups  = [aws_security_group.ecs_tasks.id]
-    subnets          = concat(aws_subnet.private_subnets_b[*].id)
+    subnets          = concat(aws_subnet.private_subnets_b[*].id, aws_subnet.private_subnets_a[*].id)
     assign_public_ip = true
 
   }
